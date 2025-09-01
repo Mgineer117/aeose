96e002560a1a1aeae0a4ec0ee319e993606b6e1d
Minjae Cho <whalswo117@gmail.com>
2025-09-01 13:26:34 -0500

    asdwqe

--- utils/get_args.py
+++ utils/get_args.py
@@ -28,7 +28,7 @@ def get_args():
     parser.add_argument("--algo-name", type=str, default="ppo", help="Disable cuda.")
     parser.add_argument("--seed", type=int, default=42, help="Batch size.")
     parser.add_argument(
-        "--num-runs", type=int, default=10, help="Number of samples for training."
+        "--num-runs", type=int, default=5, help="Number of samples for training."
     )
 
     parser.add_argument(
@@ -47,7 +47,7 @@ def get_args():
     parser.add_argument("--critic-fc-dim", type=int, nargs="+", default=[256, 256])
 
     parser.add_argument(
-        "--timesteps", type=int, default=1e6, help="Number of training epochs."
+        "--timesteps", type=int, default=3e6, help="Number of training epochs."
     )
 
     parser.add_argument(
