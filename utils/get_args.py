9f6b0193d8584796ebb81928bad58e2ae507a60a
Minjae Cho <whalswo117@gmail.com>
2025-09-17 14:02:08 -0500

    asdwqe

--- utils/get_args.py
+++ utils/get_args.py
@@ -35,7 +35,7 @@ def get_args():
     )
 
     parser.add_argument(
-        "--actor-lr", type=float, default=1e-4, help="Base learning rate."
+        "--actor-lr", type=float, default=5e-5, help="Base learning rate."
     )
     parser.add_argument(
         "--critic-lr", type=float, default=1e-4, help="Base learning rate."
@@ -67,11 +67,11 @@ def get_args():
     parser.add_argument("--num-minibatch", type=int, default=4, help="")
     parser.add_argument("--minibatch-size", type=int, default=2048, help="")
     parser.add_argument("--batch-size", type=int, default=256, help="")
-    parser.add_argument("--K-epochs", type=int, default=10, help="")
+    parser.add_argument("--K-epochs", type=int, default=50, help="")
     parser.add_argument(
         "--target-kl",
         type=float,
-        default=0.02,
+        default=0.01,
         help="Upper bound of the eigenvalue of the dual metric.",
     )
     parser.add_argument(
