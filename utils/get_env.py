24e24b043c5bdf876ffcbcada1386b3980a000cc
Minjae Cho <whalswo117@gmail.com>
2025-10-02 20:07:59 -0500

    asdqwe

--- utils/get_env.py
+++ utils/get_env.py
@@ -141,6 +141,7 @@ class DownlinkReward(GlobalReward):
     # "Data delivered" = max(0, prev_storage - current_storage) when a GS contact is open
     def calculate_reward(self, new_data_dict):
         rewards = {}
+        print(self)
         print(self.__dict__)
         for sat_id, sat in new_data_dict.items():
             print(sat_id, sat)
