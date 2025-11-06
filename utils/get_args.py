7ec7f342ee7a04cffdf8aa2d036002ea539f1dee
Minjae Cho <whalswo117@gmail.com>
2025-11-05 19:28:30 -0600

    sadqwe

--- utils/get_args.py
+++ utils/get_args.py
@@ -38,7 +38,7 @@ def get_args():
     )
 
     parser.add_argument(
-        "--actor-lr", type=float, default=1e-4, help="Base learning rate."
+        "--actor-lr", type=float, default=1e-5, help="Base learning rate."
     )
     parser.add_argument(
         "--critic-lr", type=float, default=3e-5, help="Base learning rate."
@@ -70,7 +70,7 @@ def get_args():
     parser.add_argument("--num-minibatch", type=int, default=4, help="")
     parser.add_argument("--minibatch-size", type=int, default=4096, help="")
     parser.add_argument("--batch-size", type=int, default=256, help="")
-    parser.add_argument("--K-epochs", type=int, default=20, help="")
+    parser.add_argument("--K-epochs", type=int, default=50, help="")
     parser.add_argument(
         "--target-kl",
         type=float,
