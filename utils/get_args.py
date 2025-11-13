b4e4801e6b4eb81ef375f7eba686688d7581944c
Minjae Cho <whalswo117@gmail.com>
2025-11-12 23:35:55 -0600

    asdqwe

--- utils/get_args.py
+++ utils/get_args.py
@@ -58,7 +58,7 @@ def get_args():
     parser.add_argument("--critic-fc-dim", type=int, nargs="+", default=[256, 256])
 
     parser.add_argument(
-        "--timesteps", type=int, default=1e7, help="Number of training epochs."
+        "--timesteps", type=int, default=int(1.1e7), help="Number of training epochs."
     )
 
     parser.add_argument(
